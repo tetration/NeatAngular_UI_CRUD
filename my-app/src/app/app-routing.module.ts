2a37d6fd56239d0f4ecae2dd2a3fb45b9645db45
tetration <rafael.august.b@gmail.com>
2021-01-24 20:49:36 -0300

    Added Bootstrap sass

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+//import {TaskFormComponent} from './tasks/task-form/task-form.component';
+//import
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',
+    component: HomeComponent,
+    children:[
+      //{ path: '', component: TaskListComponent },
+      //{ path: '', component: TaskFormComponent},
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './account/login/login.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { HomeComponent } from './layout/home/home.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HomeComponent,
+    AuthenticationComponent,
   ],
   imports: [
     BrowserModule,
